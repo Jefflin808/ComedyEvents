f065ec5bf057d78e182f7fa7a131132140b111ef
Lin Jianfeng(DC-IA/EAO2) <Jianfeng.Lin@boschrexroth.de>
2020-04-13 14:53:35 +0200

    create eventscontroller

---
+++ ComedyEvents/Controllers/EventsController.cs
@@ -0,0 +1,21 @@
+﻿using ComedyEvents.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComedyEvents.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private readonly IEventRepository _eventRepository;
+
+        public EventsController(IEventRepository eventRepository)
+        {
+            _eventRepository = eventRepository;
+        }
+    }
+}
