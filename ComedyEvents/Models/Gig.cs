de56187ce617a3a9dcb74f192da0b4c093779d39
Lin Jianfeng(DC-IA/EAO2) <Jianfeng.Lin@boschrexroth.de>
2020-04-15 18:03:16 +0200

    add post put delete in comediansController

--- ComedyEvents/Models/Gig.cs
+++ ComedyEvents/Models/Gig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,7 +11,11 @@ namespace ComedyEvents.Models
         public int GigId { get; set; }
         public string GigHeadline { get; set; }
         public int GigLengthInMinutes { get; set; }
+        [ForeignKey("EventId")]
+        public int EventId { get; set; }
         public Event Event { get; set; }
+        [ForeignKey("ComedianId")]
+        public int ComedianId { get; set; }
         public Comedian Comedian { get; set; }
     }
 }
